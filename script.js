ee617f2b6670af63394fef563be5d3fb5ddabf73
Prayesh007 <prayeshpatel079@gmail.com>
2024-09-02 20:33:15 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let icon = document.querySelector('.scroll-icon');
+window.addEventListener('scroll', function () {
+
+
+    if (window.scrollY > 0) {
+        icon.classList.add('hidden');
+    }
+
+});
+
+const body = document.body;
+let toggle = document.querySelectorAll('.light-theme')
+let heading = document.querySelector('.light-theme-h1');
+let buttons = document.querySelectorAll('.button-light');
+
+
+
+let themeToggle = this.document.getElementById('theme');
+
