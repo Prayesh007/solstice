f4b572f74c098d6e54868944244fb3a3a56b8a09
Prayesh007 <162706704+Prayesh007@users.noreply.github.com>
2024-09-28 19:53:34 +0530

    added toggle-logic

--- gallery.js
+++ gallery.js
@@ -1,1 +1,17 @@
+// code for toggle betn cinema & poetry
 
+
+let toggle1 = document.querySelector('.cinema-btn');
+let toggle2 = document.querySelector('.poetry-btn');
+let cinema = document.querySelector(".cinema");
+let poetry = document.querySelector(".poetry");
+
+
+toggle1.addEventListener('click', function () {
+    cinema.classList.remove('hidden');
+    poetry.classList.add('hidden');
+    toggle1.classList.add('active');
+    toggle2.classList.remove('active');
+
+
+});
